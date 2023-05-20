53eaf4f77215b160d88b7f93f548ea08067c8795
AliSiddique <alisiddique10@hotmail.com>
2023-05-20 12:28:55 +0100

    Home

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
+import Navbar from '@/components/Constants/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Constants/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
     </html>
   )
 }
