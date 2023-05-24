acb7e16990d5ec3fdff66a76da56e1a0a87fcd4a
AliSiddique <alisiddique10@hotmail.com>
2023-05-24 18:50:59 +0100

    login pricing signup footer

---
+++ frontend/src/app/user/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/components/User/Account/Forms/LoginForm'
+import React from 'react'
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <div>
+        <LoginForm />
+    </div>
+  )
+}
